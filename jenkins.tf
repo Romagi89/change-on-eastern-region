18c25f2434ea010b3750b85003953e074d2f4b18
roma <romagiri2007@gmail.com>
2021-02-27 00:02:07 -0500

    change on lines

---
+++ jenkins.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-09246ddb00c7c4fef"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
